fc149250405314e46cfe3edfac260059063ca5a6
Yichen Tao <ytao15@binghamton.edu>
2019-08-30 21:55:08 -0400

    Finish major DOBTester Finished most of DOBTester. Corrected mistakes on DOB class

--- assignment01/DateAndPlaceOfBirth.java
+++ assignment01/DateAndPlaceOfBirth.java
@@ -20,8 +20,8 @@ public class DateAndPlaceOfBirth {
 	 * @param countryBirth the country where the birth took place
 	 */
 	public DateAndPlaceOfBirth(
-			int yearOfBirth, 
-			int monthOfBirth, 
+			int yearOfBirth,
+			int monthOfBirth,
 			int dayOfBirth,
 			String cityOfBirth,
 			String stateOfBirth,
@@ -39,8 +39,8 @@ public class DateAndPlaceOfBirth {
 	 * @param countryBirth the country where the birth took place
 	 */
 	public DateAndPlaceOfBirth(
-			int yearOfBirth, 
-			int monthOfBirth, 
+			int yearOfBirth,
+			int monthOfBirth,
 			int dayOfBirth,
 			String cityOfBirth,
 			String countryBirth) {
@@ -63,7 +63,7 @@ public class DateAndPlaceOfBirth {
 	public String getPlaceOfBirth() {
 		return placeOfBirth;
 	}
-	
+
 	/**
 	 * The method olderThan returns true if the date of birth of
 	 * this DateAndPlaceOfBirth is before the date of birth of
@@ -106,11 +106,21 @@ public class DateAndPlaceOfBirth {
 	 * the same day and month as the date of birth of other and false otherwise
 	 */
 	public boolean hasSameBirthDayAs(DateAndPlaceOfBirth other) {
-		return dateOfBirth.isEqual(other.dateOfBirth);
+		boolean sameBirthday;
+		int month1 = dateOfBirth.getMonthValue();
+		int month2 = other.dateOfBirth.getMonthValue();
+		int day1 = dateOfBirth.getDayOfMonth();
+		int day2 = other.dateOfBirth.getDayOfMonth();
+		if(month1== month2 && day1 == day2){
+			sameBirthday = true;
+		}else{
+			sameBirthday = false;
+		}
+		return sameBirthday;
   //TODO complete this method
   }
 	@Override
 	public String toString() {
 		return "Date and place of birth: " + dateOfBirth + ", " + placeOfBirth;
-	}	
+	}
 }
---
+++ assignment01/PlaceDOBTester.java
@@ -0,0 +1,50 @@
+package assignment01;
+/**
+Write a class PlaceDOBTester.java with a main method to check out the DateAndPlaceOfBirth class:
+In the main method make 5 DateAndPlaceOfBirth objects 3 with birth places in the USA (with a State) and
+2 from other countries using the 2nd constructor that does not have a state parameter.
+Give two objects the same birth date and have another object with the same birthday (day and month) but a different birth year.
+Use System.out.println to print each of the 5 objects and then to print the values of the four other
+methods that are not getter methods, e.g., System.out.println(object1.hasSameBirthDayAs(object2)).
+You will need many lines to test the 4 methods, we want to see that get the right true and false values for the different combinations of the objects you made.
+*/
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import assignment01.DateAndPlaceOfBirth;
+public class PlaceDOBTester {
+	public static void main(String[] args) {
+		DateAndPlaceOfBirth DOBPlaceOne = new DateAndPlaceOfBirth(2000, 12, 25, "Binghamton", "NY", "USA");
+		DateAndPlaceOfBirth DOBPlaceTwo = new DateAndPlaceOfBirth(2000, 12, 25, "Binghamton", "NY", "USA");
+		DateAndPlaceOfBirth DOBPlaceThree = new DateAndPlaceOfBirth(2001, 12, 25, "Binghamton", "NY", "USA");
+		DateAndPlaceOfBirth DOBPlaceFour = new DateAndPlaceOfBirth(1999, 11, 24, "London", "UK");
+		DateAndPlaceOfBirth DOBPlaceFive = new DateAndPlaceOfBirth(1998, 10, 14, "London", "UK");
+		System.out.println(DOBPlaceOne);
+		System.out.println(DOBPlaceTwo);
+		System.out.println(DOBPlaceThree);
+		System.out.println(DOBPlaceFour);
+		System.out.println(DOBPlaceFive);
+		System.out.println(DOBPlaceOne.olderThan(DOBPlaceFive));			//False
+		System.out.println(DOBPlaceFive.olderThan(DOBPlaceOne));			//True
+		System.out.println(DOBPlaceOne.youngerThan(DOBPlaceFive));			//True
+		System.out.println(DOBPlaceFive.youngerThan(DOBPlaceOne));			//False
+		System.out.println(DOBPlaceOne.hasSameBirthDateAs(DOBPlaceTwo));	//True
+		System.out.println(DOBPlaceOne.hasSameBirthDateAs(DOBPlaceFive));	//False
+		System.out.println(DOBPlaceOne.hasSameBirthDayAs(DOBPlaceTwo));		//True
+		System.out.println(DOBPlaceOne.hasSameBirthDayAs(DOBPlaceThree));	//True
+		System.out.println(DOBPlaceOne.hasSameBirthDayAs(DOBPlaceFive));	//False
+
+
+		try(var output =new PrintWriter(new FileOutputStream(
+			    new File("output.txt"), true /* true means append to file */))) {
+			output.println("\nTESTS FOR Computer.java:");
+
+		//Copy all your lines above, add an extra "2" to every variable name
+		// and replace every System.out.print or System.out.println
+		// by output.print or output.println
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
